a1c4f79cd057e8edd6c4346b77d8e4dc5baca8e3
Taras <tristanporie34@gmail.com>
2025-04-03 19:53:39 +0300

    refactoring CatDTO

--- src/main/java/com/example/demo/DTO/CatDTO.java
+++ src/main/java/com/example/demo/DTO/CatDTO.java
@@ -7,7 +7,12 @@ import lombok.experimental.FieldDefaults;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CatDTO {
+    //имя котика
     String name;
+
+
     int weight;
+
+
     int age;
 }
